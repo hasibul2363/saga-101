52a4301f3ddb8d0e21142e07b38fd3374a1b7d13
Hasibul Haque <hasibul2363@gmail.com>
2019-10-30 21:13:04 +0100

    Added initial skeleton

---
+++ src/inventory/InventoryHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoolBrains.InventoryHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/payment/PaymentHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoolBrains.PaymentHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
